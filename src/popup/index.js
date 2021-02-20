97cebdd0135cb84e80d526a76b55ed3bb54f2437
cormacbeagan <cormacbeagan@gmail.com>
2021-02-20 23:30:08 +0100

    ...

--- src/popup/index.js
+++ src/popup/index.js
@@ -9,46 +9,41 @@ const negative = document.getElementById('negRating');
 let summary = {};
 
 function handleLoad() {
-  chrome.storage.local.get('events', (resp) => {
-    const data = resp.events;
-  });
-
   summarizeEvents();
-
 }
 
 window.onload = handleLoad;
 
 const clearLocalStorage = () => {
   console.log(window);
+  console.log(summary);
   //chrome.storage.local.clear();
 };
 
 deleteButton.onclick = clearLocalStorage;
 
-
-const addResults = (r1, r2)=> {
-	let result = {}
-	if (r1.pos == null) {
-		console.log('r1 was null', r1)
-		result = r2;
-	} else {
-		result.compound = (r1.compound + r2.compound);
-		result.neg = (r1.neg + r2.neg);
-		result.neu = (r1.neu + r2.neu);
-		result.pos = (r1.pos + r2.pos);
-	}
-	return result;
-}
+const addResults = (r1, r2) => {
+  let result = {};
+  if (r1.pos == null) {
+    console.log('r1 was null', r1);
+    result = r2;
+  } else {
+    result.compound = r1.compound + r2.compound;
+    result.neg = r1.neg + r2.neg;
+    result.neu = r1.neu + r2.neu;
+    result.pos = r1.pos + r2.pos;
+  }
+  return result;
+};
 
 const summarizeEvents = () => {
-	chrome.storage.local.get(['events'], (items) => {
-		sumUp(items.events);
-	});
-}
+  chrome.storage.local.get(['events'], (items) => {
+    sumUp(items.events);
+  });
+};
 
 const sumUp = (events) => {
-	for (var i = 0; i < events.length; i++) {
-		summary = addResults(summary, events[i].result)
-	}
-}
+  for (var i = 0; i < events.length; i++) {
+    summary = addResults(summary, events[i].result);
+  }
+};
