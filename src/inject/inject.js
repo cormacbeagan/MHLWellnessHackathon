bb10b1a92b0aca9b0f990a879a6638f9191bdf14
Fidelma <fidelma.beagan@gmail.com>
2021-02-20 14:19:58 +0000

    gets headlines from bbc news front page

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,6 +7,16 @@ chrome.extension.sendMessage({}, function(response) {
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
+    const headlines = Array.from(document.getElementsByClassName("gs-c-promo-heading__title"))
+
+    if(headlines.length > 0) {
+     const headlineTexts = headlines.map((headline)=> {
+       return headline.innerHTML
+     })
+     console.log(headlineTexts);
+    } else {
+      console.log("nothing here");
+    }
 
 	}
 	}, 10);
