3859d03de2379e0ba5d6439f97067b1fe1592d92
cormacbeagan <cormacbeagan@gmail.com>
2021-02-20 18:46:55 +0100

    added text scraping and anaysis to inject.js

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,31 +1,65 @@
 const vader = require('vader-sentiment');
 import './inject.css';
 
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
-    const headlines = Array.from(document.getElementsByClassName("gs-c-promo-heading__title"))
+chrome.extension.sendMessage({}, function (response) {
+  var readyStateCheckInterval = setInterval(function () {
+    if (document.readyState === 'complete') {
+      clearInterval(readyStateCheckInterval);
 
-    if(headlines.length > 0) {
-     const headlineTexts = headlines.map((headline)=> {
-       return headline.innerHTML
-     })
-     headlineTexts.forEach((headline) =>{
-       const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(headline);
-       console.log(intensity);
-      //  console.log(headline, `score: ${intensity.compound}`);
-     })
-     
-    } else {
-      console.log("nothing here");
-    }
       // ----------------------------------------------------------
+      // This part of the script triggers when page is done loading
+      console.log('Page loaded');
+      // ----------------------------------------------------------
+
+      // load each P tag and create an array of objects with intensity and position on the y axis
+      const paragraphs = Array.from(document.querySelectorAll('p'));
+      let paraObjects = [];
+      let objectsToSend = [];
+
+      if (paragraphs.length > 0) {
+        paraObjects = paragraphs.map((para, index) => {
+          return {
+            id: index,
+            text: para.innerText,
+            offset: para.offsetTop,
+            intensity: vader.SentimentIntensityAnalyzer.polarity_scores(
+              para.innerText
+            ),
+          };
+        });
+        handleScroll();
+      } else {
+        console.log('nothing here');
+      }
+
+      function handleScroll() {
+        const top = window.scrollY;
+        const bottom = top + window.innerHeight;
+        paraObjects.map((object) => {
+          if (object.offset < bottom && object.offset > top) {
+            objectsToSend.push(object);
+            paraObjects = paraObjects.filter((item) => item.id !== object.id);
+          }
+        });
+      }
+
+      window.addEventListener('scroll', debounce(handleScroll));
     }
   }, 10);
 });
