714eacf6d4d5cdb285ff3796e58a0a9ce09d1ca4
kaklin <filip.kaklin@gmail.com>
2021-02-20 22:22:03 +0000

    add summarizing functions

--- src/popup/index.js
+++ src/popup/index.js
@@ -6,10 +6,15 @@ const positive = document.getElementById('posRating');
 const neutral = document.getElementById('neuRating');
 const negative = document.getElementById('negRating');
 
+let summary = {};
+
 function handleLoad() {
   chrome.storage.local.get('events', (resp) => {
     const data = resp.events;
   });
+
+  summarizeEvents();
+
 }
 
 window.onload = handleLoad;
@@ -20,3 +25,30 @@ const clearLocalStorage = () => {
 };
 
 deleteButton.onclick = clearLocalStorage;
+
+
+const addResults = (r1, r2)=> {
+	let result = {}
+	if (r1.pos == null) {
+		console.log('r1 was null', r1)
+		result = r2;
+	} else {
+		result.compound = (r1.compound + r2.compound);
+		result.neg = (r1.neg + r2.neg);
+		result.neu = (r1.neu + r2.neu);
+		result.pos = (r1.pos + r2.pos);
+	}
+	return result;
+}
+
+const summarizeEvents = () => {
+	chrome.storage.local.get(['events'], (items) => {
+		sumUp(items.events);
+	});
+}
+
+const sumUp = (events) => {
+	for (var i = 0; i < events.length; i++) {
+		summary = addResults(summary, events[i].result)
+	}
+}
